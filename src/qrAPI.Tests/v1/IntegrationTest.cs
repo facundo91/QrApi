f7bb2853c44007f694fb3f9d40eb5819a7a41fc9
Facundo Aita <facundo91@gmail.com>
2020-06-15 15:32:47 -0300

    Changing structure

---
+++ src/qrAPI.Tests/v1/IntegrationTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using qrAPI.Contracts.v1.Requests;
+using qrAPI.DAL.Data.EFData.Contexts;
+using qrAPI.Sdk.v1;
+using Refit;
+using Simple.OData.Client;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace qrAPI.Tests.v1
+{
+    public class IntegrationTest : IDisposable
+    {
+        private readonly IConfigurationRoot configuration;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IIdentityApi _identityApi;
+        protected readonly HttpClient TestClient;
+        protected readonly ODataClient ODataClient;
+
+        protected IntegrationTest()
+        {
+            configuration = GetTestConfiguration();
+            var appFactory = GetWebApplicationFactory();
+            _serviceProvider = appFactory.Services;
+            using var serviceScope = _serviceProvider.CreateScope();
+            serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.EnsureCreated();
+            TestClient = appFactory.CreateClient();
+            _identityApi = RestService.For<IIdentityApi>(TestClient);
+            var relativeUri = new Uri("odata/", UriKind.Relative);
+            ODataClient = new ODataClient(new ODataClientSettings(TestClient, relativeUri));
+        }
+
+        private WebApplicationFactory<Startup> GetWebApplicationFactory() =>
+            IsLocalDbEnvironmentVariable()
+                ? new WebApplicationFactory<Startup>()
+                : new WebApplicationFactory<Startup>().WithWebHostBuilder(builder =>
+                {
+                    builder.UseConfiguration(configuration);
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+                        services.AddDbContext<ApplicationDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDB");
+                        });
+                    });
+                });
+
+        private static IConfigurationRoot GetTestConfiguration()
+        {
+            var dir = AppContext.BaseDirectory;
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(dir)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddEnvironmentVariables();
+            var qwe = builder.Build();
+            return qwe;
+        }
+
+        private bool IsLocalDbEnvironmentVariable() => configuration.GetValue<bool>("localdb");
+
+        protected async Task RegisterAsync(string email = "test@integration.com", string password = "SomePass1234!")
+        {
+            var response = await _identityApi.RegisterAsync(new UserRegistrationRequest
+            {
+                Email = email,
+                FirstName = "first Name",
+                LastName = "last Name",
+                Password = password
+            });
+            TestClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", response.Content.Token);
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = _serviceProvider.CreateScope();
+            serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.EnsureDeleted();
+        }
+
+    }
+}
---
+++ src/qrAPI/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+using qrAPI.Contracts.v1.Responses;
+using qrAPI.Installers;
+using static Microsoft.OData.ODataUrlKeyDelimiter;
+
+namespace qrAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.InstallServicesInAssembly(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+        {
+            app.AddHealthChecks();
+            //app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseResponseCaching();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // the following will not work as expected
+                // BUG: https://github.com/OData/WebApi/issues/1837
+                // routeBuilder.SetDefaultODataOptions( new ODataOptions() { UrlKeyDelimiter = Parentheses } );
+                endpoints.ServiceProvider.GetRequiredService<ODataOptions>().UrlKeyDelimiter = Parentheses;
+                endpoints.MapControllers();
+                endpoints.EnableDependencyInjection();
+                endpoints.Select().Filter().OrderBy().Count().Expand().MaxTop(10);
+                endpoints.MapODataRoute("odata", "odata", GetEdmModel());
+            });
+            app.AddSwagger(provider);
+        }
+
+        private IEdmModel GetEdmModel()
+        {
+            var odataBuilder = new ODataConventionModelBuilder();
+            odataBuilder.EntitySet<PetResponse>("Pets").EntityType.HasKey(o => o.Id);
+            odataBuilder.EntitySet<QrResponse>("Qrs").EntityType.HasKey(o => o.Id);
+
+            return odataBuilder.GetEdmModel();
+        }
+    }
+}
